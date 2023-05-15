8e8e591410c53520931f76f1a10efad858aee559
jairdev <devalfredomoscoso@gmail.com>
2023-05-15 19:13:47 -0400

    Add process content

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -75,29 +75,128 @@ function Home() {
 
   useEffect(() => {
     if (isMounted) {
+      const split = gsap.utils.toArray("[data-text]").map((node, i) => {
+        const results = Splitting({ target: node.current, by: "chars" });
+        return results[i];
+      });
+
+      split.forEach((letters) => {
+        letters.chars.forEach((l) => {
+          const randomPosition = () => gsap.utils.random(-200, 200);
+          gsap.set(l.parentNode, { perspective: 1000 });
+          gsap.set(l, {
+            opacity: 0,
+            translateY: randomPosition(),
+            translateX: randomPosition(),
+            z: randomPosition(),
+          });
+          ScrollTrigger.batch(l, {
+            onEnter: (batch) =>
+              gsap.to(batch, {
+                // ease: "power1.out",
+                opacity: 1,
+                translateY: 0,
+                translateX: 0,
+                z: 0,
+              }),
+            onLeaveBack: (batch) =>
+              gsap.to(batch, {
+                // ease: "power1.out",
+                opacity: 0,
+                translateY: randomPosition(),
+                translateX: randomPosition(),
+                z: randomPosition(),
+              }),
+            start: "top 95%",
+            // markers: true,
+          });
+        });
+      });
+
+      // split.forEach((chars) => {
+      //   // console.log(chars.chars);
+      //   chars.chars.forEach((letter) => {
+      //     gsap.set(letter.parentNode, { perspective: 1000 });
+      //     gsap.fromTo(
+      //       chars.chars,
+      //       {
+      //         "will-change": "opacity, transform",
+      //         // opacity: 0,
+      //         // rotateX: () => gsap.utils.random(-120, 120),
+      //         // rotateX: () => 120,
+      //         // translateX: gsap.utils.random(-100, 100),
+      //         // translateY: gsap.utils.random(-100, 100),
+      //         z: () => gsap.utils.random(-200, 200),
+      //       },
+      //       {
+      //         // ease: "none",
+      //         // opacity: 1,
+      //         // rotateX: 0,
+      //         // translateX: 0,
+      //         // translateY: 0,
+      //         // translate: (0, 0),
+      //         z: 0,
+      //         // stagger: { each: 0.02, from: "random" },
+      //         scrollTrigger: {
+      //           trigger: letter,
+      //           start: "top 80%",
+      //           end: "bottom 40%",
+      //           scrub: true,
+      //           markers: true,
+      //         },
+      //       }
+      //     );
+      //   });
+      // });
+
+      // gsap
+      //   .timeline({
+      //     scrollTrigger: {
+      //       trigger: "#circle-blur",
+      //       start: "top 65% ",
+      //       end: "100% -10%",
+      //       scrub: true,
+      //       // markers: true,
+      //     },
+      //   })
+      //   .to("#circle-blur", {
+      //     ease: "sine.out",
+      //     duration: 0.7,
+      //     opacity: 1,
+      //     y: 280,
+      //   });
+    }
+  }, [isMounted]);
+
+  useEffect(() => {
+    if (isMounted) {
       const pin = gsap.timeline({
         scrollTrigger: {
           trigger: "[data-pin]",
           start: "top 10%",
-          end: "bottom 60%",
+          end: "bottom 45%",
           pin: true,
           scrub: true,
           // markers: true,
         },
       });
-      const be = gsap.timeline({
-        scrollTrigger: {
-          trigger: "[data-wrap-benefits]",
-          start: "top 100%",
-          end: "10% 70%",
-          scrub: true,
-          // markers: true,
-        },
+      pin.from("[data-wrap-benefits]", {
+        // opacity: 0,
+        y: 500,
       });
-      be.from("[data-wrap-benefits]", {
-        y: 300,
-        opacity: 0,
-      });
+      // const be = gsap.timeline({
+      //   scrollTrigger: {
+      //     trigger: "[data-wrap-benefits]",
+      //     start: "top 100%",
+      //     end: "10% 70%",
+      //     scrub: true,
+      //     markers: true,
+      //   },
+      // });
+      // be.from("[data-wrap-benefits]", {
+      //   y: 300,
+      //   // opacity: 0,
+      // });
       // pin.to(pinBenefits.current, {
       //   // yPercent: 100,
       //   // duration: 10,
@@ -127,141 +226,30 @@ function Home() {
       // });
 
       const cardsArr = gsap.utils.toArray("[data-card]").map((card) => card);
-      // console.log(cardsArr);
+
       const widthProcess = pinProcess.current.getBoundingClientRect().width;
-      console.log(widthProcess);
+
+      // it's work -----------------!!!!!!!!
+
       gsap.from(cardsArr, {
         // duration: 5,
+        x: widthProcess,
+        opacity: 0,
         stagger: {
           // ease: "none",
-          // each: 0.5,
-          amount: 4,
+          each: 0.5,
+          // amount: 4,
         },
         scrollTrigger: {
           trigger: "[data-pin-process]",
-          markers: true,
           start: "top 10%",
           // end: `bottom -=${widthProcess}`,
-          end: `bottom top`,
+          end: `bottom -=1000`,
           pin: "[data-pin-process]",
           scrub: true,
+          // markers: true,
         },
-        opacity: 0,
-        x: widthProcess,
       });
-    }
-  }, [isMounted]);
-
-  useEffect(() => {
-    if (isMounted) {
-      const split = gsap.utils.toArray("[data-text]").map((node, i) => {
-        const results = Splitting({ target: node.current, by: "chars" });
-        return results[i];
-      });
-      split.forEach((letters) => {
-        letters.chars.forEach((l) => {
-          const randomPosition = () => gsap.utils.random(-150, 150);
-          gsap.set(l, {
-            opacity: 0,
-            translateY: randomPosition(),
-            translateX: randomPosition(),
-          });
-        });
-      });
-    }
-  }, [isMounted]);
-
-  useEffect(() => {
-    if (isMounted) {
-      // console.log(refText.current);
-      const split = gsap.utils.toArray("[data-text]").map((node, i) => {
-        const results = Splitting({ target: node.current, by: "chars" });
-        // console.log(results[i]);
-        return results[i];
-      });
-
-      split.forEach((letters) => {
-        // console.log(letters.chars);
-        // gsap.to(letters.chars, {
-        //   ease: "none",
-        //   translateY: 0,
-        //   translateX: 0,
-        // });
-        letters.chars.forEach((l) => {
-          const randomPosition = () => gsap.utils.random(-150, 150);
-          ScrollTrigger.batch(l, {
-            onEnter: (batch) =>
-              gsap.to(batch, {
-                ease: "power1.out",
-                opacity: 1,
-                translateY: 0,
-                translateX: 0,
-              }),
-            onLeaveBack: (batch) =>
-              gsap.to(batch, {
-                ease: "power1.out",
-                opacity: 0,
-                translateY: randomPosition(),
-                translateX: randomPosition(),
-              }),
-            start: "top 100%",
-            // markers: true,
-          });
-        });
-      });
-
-      // split.forEach((chars) => {
-      //   // console.log(chars.chars);
-      //   chars.chars.forEach((letter) => {
-      //     gsap.set(letter.parentNode, { perspective: 1000 });
-      //     gsap.fromTo(
-      //       chars.chars,
-      //       {
-      //         "will-change": "opacity, transform",
-      //         // opacity: 0,
-      //         // rotateX: () => gsap.utils.random(-120, 120),
-      //         // rotateX: () => 120,
-      //         // translateX: gsap.utils.random(-100, 100),
-      //         // translateY: gsap.utils.random(-100, 100),
-      //         // z: () => gsap.utils.random(-200, 200),
-      //       },
-      //       {
-      //         // ease: "none",
-      //         // opacity: 1,
-      //         // rotateX: 0,
-      //         // translateX: 0,
-      //         // translateY: 0,
-      //         // translate: (0, 0),
-      //         // z: 0,
-      //         // stagger: { each: 0.02, from: "random" },
-      //         // scrollTrigger: {
-      //         //   trigger: letter,
-      //         //   start: "top 80%",
-      //         //   end: "bottom 40%",
-      //         //   scrub: true,
-      //         //   markers: true,
-      //         // },
-      //       }
-      //     );
-      //   });
-      // });
-
-      // gsap
-      //   .timeline({
-      //     scrollTrigger: {
-      //       trigger: "#circle-blur",
-      //       start: "top 65% ",
-      //       end: "100% -10%",
-      //       scrub: true,
-      //       // markers: true,
-      //     },
-      //   })
-      //   .to("#circle-blur", {
-      //     ease: "sine.out",
-      //     duration: 0.7,
-      //     opacity: 1,
-      //     y: 280,
-      //   });
     }
   }, [isMounted]);
 
@@ -434,10 +422,10 @@ function Home() {
                 Que obtendrás
               </h2>
               <p className={styles.youGetDescription}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
-                voluptates earum ipsum odio, aspernatur ducimus corrupti
-                necessitatibus magnam eligendi eius impedit voluptatem ipsam
-                iure! Deserunt laudantium molestiae enim natus ipsum.
+                Mi enfoque en el desarrollo web se centra en ofrecer resultados
+                que sean importantes para tu negocio. Me tomo el tiempo para
+                comprender tus necesidades, objetivos y así crear una solución
+                personalizada adaptada a tus necesidades.
               </p>
             </div>
 
@@ -461,7 +449,10 @@ function Home() {
                 <p className={styles.benefitsHeader}>
                   La seguridad es primordial
                 </p>
-                <div className={styles.benefitsCard}>
+                <div
+                  data-benefitsCard="data-benefitsCard"
+                  className={styles.benefitsCard}
+                >
                   Utilizo herramientas que proporcionan una infraestructura
                   donde tus datos estarán 100% seguros.
                 </div>
@@ -471,17 +462,22 @@ function Home() {
                 <p className={styles.benefitsHeader}>
                   Optimización básica de motores de búsqueda (SEO)
                 </p>
-                <div className={styles.benefitsCard}>
-                  Utilizo herramientas que proporcionan una infraestructura
-                  donde tus datos estarán 100% seguros.
+                <div
+                  data-benefitsCard="data-benefitsCard"
+                  className={styles.benefitsCard}
+                >
+                  El SEO básico consiste en utilizar buenas prácticas para
+                  aparecer en los principales buscadores web.
                 </div>
               </div>
               {/* maintenance */}
               <div className={styles.wrapperCardBenefits}>
                 <p className={styles.benefitsHeader}>Soporte y mantenimiento</p>
-                <div className={styles.benefitsCard}>
-                  Utilizo herramientas que proporcionan una infraestructura
-                  donde tus datos estarán 100% seguros.
+                <div
+                  data-benefitsCard="data-benefitsCard"
+                  className={styles.benefitsCard}
+                >
+                  Soporte y mantenimiento sin coste adicional por 6 meses.
                 </div>
               </div>
             </div>
@@ -534,44 +530,51 @@ function Home() {
               </h2>
 
               <p className={styles.processDescription}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
-                voluptates earum ipsum odio, aspernatur ducimus corrupti
+                Tengo los conocimientos y la experiencia para ayudarte a lograr
+                tus objetivos.
               </p>
             </div>
 
             <div ref={pinProcess} className={styles.wrapperProcess}>
               <div data-card="data-card" className={styles.cardProcess}>
-                <h3 className={styles.appContentTitleIDoWork}>Landing pages</h3>
+                <h3 className={styles.appContentTitleIDoWork}>01. Descubrir</h3>
                 <div className={styles.appContentTitleIDoWorkSubTitle}>
-                  Sitio web de una página, ideal para emprendedores, pequeñas
-                  empresas, presentación de productos.
+                  Tendremos nuestra primera reunión para saber mas sobre tu
+                  proyecto, te daré una aproximación de cuanto tiempo tomaría y
+                  el presupuesto del proyecto.
+                </div>
+              </div>
+              <div data-card="data-card" className={styles.cardProcess}>
+                <h3 className={styles.appContentTitleIDoWork}>02. Diseño</h3>
+                <div className={styles.appContentTitleIDoWorkSubTitle}>
+                  Luego de saber sobre tu genial idea, comenzaré mi proceso de
+                  diseño, al finalizar mi propuesta nos volvemos a reunir para
+                  intercambiar ideas y si estas de acuerdo con el diseño vamos
+                  al siguiente punto.
                 </div>
               </div>
               <div data-card="data-card" className={styles.cardProcess}>
                 <h3 className={styles.appContentTitleIDoWork}>
-                  Sitios web de múltiples de páginas
+                  03. Desarrollo
                 </h3>
                 <div className={styles.appContentTitleIDoWorkSubTitle}>
-                  Autenticación (registro/inicio de sesión), integración con
-                  bases de datos.
+                  Utilizando las herramientas con las que trabajo, comienzo con
+                  el proceso de construcción del sitio web, en este punto
+                  seguiremos en contacto porque tu también formas parte del
+                  proceso.
                 </div>
               </div>
               <div data-card="data-card" className={styles.cardProcess}>
                 <h3 className={styles.appContentTitleIDoWork}>
-                  Sitios web de múltiples de páginas
+                  04. Lanzamiento
                 </h3>
-                <div className={styles.appContentTitleIDoWorkSubTitle}>
-                  Autenticación (registro/inicio de sesión), integración con
-                  bases de datos.
-                </div>
-              </div>
-              <div data-card="data-card" className={styles.cardProcess}>
-                <h3 className={styles.appContentTitleIDoWork}>
-                  Sitios web de múltiples de páginas
-                </h3>
-                <div className={styles.appContentTitleIDoWorkSubTitle}>
-                  Autenticación (registro/inicio de sesión), integración con
-                  bases de datos.
+                <div
+                  data-text1="data-text1"
+                  className={styles.appContentTitleIDoWorkSubTitle}
+                >
+                  Publicaré tu sitio web, haré las pruebas necesarias para
+                  asegurarme de entregar un trabajo de calidad, si todo marcha
+                  bien, serás otro cliente satisfecho.
                 </div>
               </div>
             </div>
